6c01ed3feed611590b185a890d81e5ad12a71210
Igor-PC\Igor <zas.i.e@yandex.ru>
2024-09-15 21:22:37 +0300

    ДЗ с классом Stack первый вариант

---
+++ ClassCollectionStack/ClassCollectionStack/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace ClassCollectionStack
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            var stack = new Stack("a", "b", "c");
+            stack.Merge(new Stack("1", "2", "3"));
+
+            var stackConcat1 = Stack.Concat(new Stack("a", "b", "c"), new Stack("1", "2", "3"), new Stack("А", "Б", "В"));
+
+            foreach (var item in stackConcat1)
+            {
+                Console.Write($"{item}, ");
+            }
+
+            foreach (var item in stack)
+            {
+                Console.Write($"{item}, ");
+            }
+        }
+    }
+}
---
+++ ClassCollectionStack/ClassCollectionStack/Stack.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+
+namespace ClassCollectionStack
+{
+    public class Stack : IEnumerable
+    {
+        private readonly List<string> _list = new();
+
+        public Stack(params string[] val)
+        {
+
+            foreach (var item in val)
+            {
+                _list.Add(item);
+            }
+        }
+
+        public string Pop()
+        {
+            string element;
+
+            if (Size > 0)
+            {
+                element = _list[^1];
+                _list.Remove(_list[^1]);
+            }
+            else
+            {
+                throw new Exception("Стек пустой");
+            }
+
+            return element;
+        }
+
+        public void Add(string elem)
+        {
+            _list.Add(elem);
+        }
+
+        public int Size => _list.Count;
+
+        public string Top => _list.LastOrDefault();
+
+        public static Stack Concat(params Stack[] stacks)
+        {
+            var newStack = new Stack();
+
+            foreach (var item in stacks)
+            {
+                var stackSize = item.Size;
+
+                for (var i = 0; i < stackSize; i++)
+                {
+                    newStack.Add(item.Pop());
+                }
+            }
+            return newStack;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
